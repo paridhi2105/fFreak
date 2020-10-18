d1f0df448969dbfbaad3a79f3522c7416af3b811
paridhi2105 <55832462+paridhi2105@users.noreply.github.com>
2020-10-18 20:12:49 +0530

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -27,20 +27,35 @@ def recommendation(ht,wt):
     df['Unit_cal']=df['Calories']/df['weight']
 
     bmi_dict={'22':0.25,'21':0.22,'26':0.25,'30':0.30,'15':0.12,'19':0.20,'42':0.35} #'bmi':'cal/g'
+    #bmi_dict can be used for LR to predict cal for given bmi
+    #bmi_and_cal=pd.DataFrame()
+    #bmi_and_cal['bmi']=['22','21','26','30','15','19','42']
+    #bmi_and_cal['cals']=[0.25,0.22,0.25,0.30,0.12,0.20,0.35]
+    #features=bmi_and_cal['bmi']
+    #features=np.asarray(features)
+    #features=features.reshape((len(features),1))
+    #labels=bmi_and_cal['cals']
+    #from sklearn.linear_model import LinearRegression
+    #model=LinearRegression()
+    #model.fit(features,labels)
 
+    
     ht=int(ht)
     wt=int(wt)
     bmi=0
 
     bmi=str(int(wt/(ht*ht)))
     unitcal=bmi_dict[bmi]
+    #bmi=np.asarray(bmi).reshape((1,1))
+    #unitcal=model.predict(bmi) 
+    #unitcal=int(unitcal)
 
     idx=[]
     iteration= list(df['Unit_cal'])
     for val in iteration:
         if (val >= (unitcal-0.0001)) or (val<=(unitcal+0.0001)):
             idx.append(iteration.index(val))
-    idx=np.array(idx) 
+    idx=np.array(idx)
     idx=np.unique(idx)
     idx=list(idx)
     df.iloc[idx,0]
